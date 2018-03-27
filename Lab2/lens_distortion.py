660c32a81b1a51eee689ee1af31ecf47db868261
samor <dingurth.d@gmail.com>
2018-03-27 19:36:19 +0200

    Lab 1,2 and 3 done

---
+++ Lab2/lens_distortion.py
@@ -0,0 +1,148 @@
+import numpy as np
+from PIL import Image, ImageDraw
+from scipy import ndimage as ndim
+from scipy.misc import imsave
+
+def load_calib_data(filename):
+    with open(filename, 'r') as f:
+        [f1, s, f2, u, v] = f.readline().split()
+        K = np.array( [[np.float(f1), np.float(s) , np.float(u)],
+                       [0.0         , np.float(f2), np.float(v)],
+                       [0.0         , 0.0         , 1.0        ]])
+        f.readline() # read empty line
+        distortion = np.array([np.float(x) for  x in f.readline().split()])
+        f.readline() # read empty line
+
+        extrinsic_matrices = []
+        for _ in range(5):
+            l = []
+            for _ in range(3):
+                l.append(f.readline().split())
+            R = np.array(l).astype(np.float)
+            t = np.array(f.readline().split()).astype(np.float)
+            extrinsic_matrices.append(np.concatenate((R, t[:, np.newaxis]), axis=1))
+            f.readline()
+    
+    return K, distortion, extrinsic_matrices
+
+K, distrt, extrinsic_matrices = load_calib_data('data/Calib.txt')
+
+def load_corners_points(filename):
+    with open(filename, 'r') as f:
+        points = []
+        for line in f:
+            points.append(np.array(line.split()).astype(np.float))
+    points = np.array(points).reshape((-1,2))
+    return np.concatenate((points, np.zeros((points.shape[0],1))), axis=1)
+
+points = load_corners_points('data/Model.txt')
+
+def to_homogenous(data):
+    return np.concatenate((data, np.ones((data.shape[0], 1))), axis=1)
+
+def from_homogenous(data):
+    return (data / data[:,-1, np.newaxis])[:,:-1]
+
+def draw_image_with_points(filename, points, new_filename):
+    im = Image.open(filename)
+    draw = ImageDraw.Draw(im)
+    width = 3
+    for point in points:
+        x = point[0]
+        y = point[1]
+        draw.ellipse((x - width, y - width, x + width, y + width), fill = 200)
+        draw.point(point[:-1], fill = 256)
+    im.save(new_filename, 'GIF')
+
+def distort_points(points, distortion_params):
+    r_square = np.sum(points **2, axis=1)[:,np.newaxis]
+    distorted_points = points * (1 + distortion_params[0] * r_square + distortion_params[1] * r_square * r_square)
+    return distorted_points
+
+
+# task 1
+P1 = K.dot(extrinsic_matrices[0])
+P2 = K.dot(extrinsic_matrices[1])
+
+hom_points = to_homogenous(points)
+points2d = from_homogenous(P1.dot(hom_points.T).T)
+draw_image_with_points('data/UndistortIm1.gif', points2d, 'data/UndIm1withPoints.gif')
+draw_image_with_points('data/CalibIm1.gif', points2d, 'data/CalibIm1withPoints.gif')
+
+dist_points2d = distort_points(from_homogenous(extrinsic_matrices[0].dot(hom_points.T).T), distrt)
+p = to_homogenous(dist_points2d)
+p = K.dot(p.T)
+dist_points2d = from_homogenous(p.T)
+draw_image_with_points('data/CalibIm1.gif', dist_points2d, 'data/DistIm1withPoints.gif')
+
+
+
+points2d = from_homogenous(P2.dot(hom_points.T).T)
+draw_image_with_points('data/UndistortIm2.gif', points2d, 'data/UndIm2withPoints.gif')
+draw_image_with_points('data/CalibIm2.gif', points2d, 'data/CalibIm2withPoints.gif')
+
+dist_points2d = distort_points(from_homogenous(extrinsic_matrices[1].dot(hom_points.T).T), distrt)
+p = to_homogenous(dist_points2d)
+p = K.dot(p.T)
+dist_points2d = from_homogenous(p.T)
+draw_image_with_points('data/CalibIm2.gif', dist_points2d, 'data/DistIm2withPoints.gif')
+
+#task 2
+
+def undistort_image(filename, distortion, intrinsic_matrix, extrinsic_matrix, new_filename):
+    image = ndim.imread(filename)
+
+    image0_points, image1_points = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0]))
+    sample = np.stack((image0_points, image1_points), axis=2).reshape((-1,2))
+
+    hom_sample = to_homogenous(sample)
+    inv_K = np.linalg.inv(intrinsic_matrix)
+    norm_sample = inv_K.dot(hom_sample.T).T
+    distorted_sample = distort_points(from_homogenous(norm_sample), distortion)
+
+    coordinates = from_homogenous(intrinsic_matrix.dot(to_homogenous(distorted_sample).T).T)
+    coordinates = np.fliplr(coordinates)
+
+    mappingR = ndim.map_coordinates(image[:,:,0], coordinates.T, order=1).reshape(image.shape[0:-1])
+    mappingG = ndim.map_coordinates(image[:,:,1], coordinates.T, order=1).reshape(image.shape[0:-1])
+    mappingB = ndim.map_coordinates(image[:,:,2], coordinates.T, order=1).reshape(image.shape[0:-1])
+
+    new_image = np.stack((mappingR, mappingG, mappingB), axis=2)
+
+    imsave(new_filename, new_image)
+
+
+
+for i in range(5):
+    undistort_image('data/CalibIm' + str(i+1) + '.gif', distrt, K, extrinsic_matrices[0], 'data/myUndistortedIm' + str(i+1) + '.gif')
+    P = K.dot(extrinsic_matrices[i])
+    hom_points = to_homogenous(points)
+    points2d = from_homogenous(P.dot(hom_points.T).T)
+    draw_image_with_points('data/myUndistortedIm' + str(i+1) + '.gif', points2d, 'data/myUndistortedIm' + str(i+1) + 'withPoints.gif')
+
+# task 3
+l = []
+lb = []
+for i in range(5):
+    observed_points = load_corners_points('data/data' + str(i+1) + '.txt')
+    P = K.dot(extrinsic_matrices[i])
+    hom_points = to_homogenous(points)
+    points2d = from_homogenous(P.dot(hom_points.T).T)
+    center = K[0:2, 2]
+    norm_points = from_homogenous(np.linalg.inv(K).dot(to_homogenous(points2d).T).T)
+    x_y_square = np.array([np.sum(norm_points**2, axis=1), np.sum(norm_points**2, axis=1)**2]).T
+
+    M = np.zeros((2*observed_points.shape[0],2))
+    M[::2,:] = (points2d[:,0] - center[0])[:,np.newaxis] * x_y_square
+    M[1::2,:] = (points2d[:,1] - center[1])[:,np.newaxis] * x_y_square
+
+    b = np.zeros(2*observed_points.shape[0])
+    b[::2] = observed_points[:,0] - points2d[:,0]
+    b[1::2] = observed_points[:,1] - points2d[:,1]
+    l.append(M)
+    lb.append(b)
+
+A = np.concatenate(l, axis=0)
+b = np.concatenate(lb, axis=0)
+
+result,_,_,_ = np.linalg.lstsq(A, b)
---
+++ Lab3/fundamental_matrix.py
@@ -0,0 +1,209 @@
+from scipy import io
+import numpy as np
+from PIL import Image, ImageDraw
+from plyfile import PlyData, PlyElement
+
+x = io.loadmat('data/compEx1data.mat')['x']
+
+points_image_1 = x[0,0].T
+points_image_2 = x[1,0].T
+
+def get_linear_solution(points_image_1, points_image_2):
+    points_number = points_image_1.shape[0]
+    A = np.zeros((points_number, 9))
+    A[:,:3] = points_image_1[:,0, np.newaxis] * points_image_2
+    A[:,3:6] = points_image_1[:,1, np.newaxis] * points_image_2
+    A[:,6:] = points_image_1[:,2, np.newaxis] * points_image_2
+
+    _, _, V = np.linalg.svd(A, full_matrices=True)
+
+    M = V[-1,:].reshape((3,3))
+
+    return M
+
+def fundamental_matrix(points_image_1, points_image_2):
+    '''Function returns fundamental matrix 
+    for x from image_2 and x' from image_1 '''
+    F = get_linear_solution(points_image_1, points_image_2)
+    u,s,v = np.linalg.svd(F)
+    s[-1] = 0
+    F = u.dot(np.diag(s).dot(v))
+
+    return F
+
+def essential_matrix(points_image_1, points_image_2, K):
+    '''Function returns essential matrix 
+    for x from image_2 and x' from image_1 '''
+    inv_K = np.linalg.inv(K)
+    norm_points1 = inv_K.dot(points_image_1.T).T
+    norm_points2 = inv_K.dot(points_image_2.T).T
+    E = get_linear_solution(norm_points1, norm_points2)
+    u,s,v = np.linalg.svd(E)
+    s = np.diag([(s[0] + s[1]) / 2., (s[0] + s[1]) / 2., 0.])
+    E = u.dot(s.dot(v))
+    return E
+
+def draw_epipolar_line(point, F, draw, width):
+    l = F.dot(point)
+    l = l / (-l[1])
+    draw.line((0, l[2],width, width * l[0] + l[2]), fill=128, width=5)
+    return draw
+
+def get_normalization_matrix(points):
+    meanX = np.mean(points[:,0])
+    meanY = np.mean(points[:,1])
+    stdX = np.std(points[:,0])
+    stdY = np.std(points[:,1])
+    sqrt2 = np.sqrt(2)
+    scale_matrix = np.array([
+                [sqrt2 / stdX, 0,            0],
+                [0,            sqrt2 / stdY, 0],
+                [0,            0,            1]])
+    translation_matrix = np.array([
+                        [1, 0, -meanX],
+                        [0, 1, -meanY],
+                        [0, 0,  1    ]])
+    T = scale_matrix.dot(translation_matrix)
+    return T
+
+def compute_3d_point(P1, P2, norm_point_1, norm_point_2):
+    A = np.array([
+        norm_point_1[0] * P1[2,:] - P1[0,:],
+        norm_point_1[1] * P1[2,:] - P1[1,:],
+        norm_point_2[0] * P2[2,:] - P2[0,:],
+        norm_point_2[1] * P2[2,:] - P2[1,:]])
+
+    _,_,v = np.linalg.svd(A)
+    return v[-1,:]
+
+def number_of_good_points(P, points):
+    camera_Z_direction = P.dot(np.array([0., 0., 10000., 1.]))[2]
+    number_of_good_points = len(points[P.dot(points.T).T[:,2] * camera_Z_direction > 0])
+    return number_of_good_points
+
+def compare_matrix(P1, P2, points_image_1, points_image_2):
+    hom_points_3d = np.array([compute_3d_point(P1, P2, point1, point2) for point1, point2 in zip(points_image_1, points_image_2)])
+    points_3d = hom_points_3d / hom_points_3d[:,-1,np.newaxis]
+    return number_of_good_points(P1, points_3d) + number_of_good_points(P2, points_3d)
+
+
+def get_projection_matrices_from_essential(E, points_image_1, points_image_2):
+    ''' Points sholud be normalized (multiplied by inverse of K) '''
+
+    P1 = np.eye(3, M=4)
+    U,S,Vt = np.linalg.svd(E)
+    U *= S[0]
+    S /= S[0]
+    W = np.array([[0., -1., 0.],
+                  [1.,  0., 0.],
+                  [0.,  0., 1.]])
+    u3 = U[:,2]
+    P2s = [np.hstack((U.dot(W.dot(Vt)), u3[:,np.newaxis])),
+           np.hstack((U.dot(W.dot(Vt)), -u3[:,np.newaxis])),
+           np.hstack((U.dot(W.T.dot(Vt)), u3[:,np.newaxis])),
+           np.hstack((U.dot(W.T.dot(Vt)), -u3[:,np.newaxis]))]
+    P2 = P2s[np.argmax([compare_matrix(P1, P, points_image_1, points_image_2) for P in P2s])]
+
+    return P1, P2
+
+def triangulate_points(E, points_image_1, points_image_2, filename):
+    ''' Points sholud be normalized (multiplied by inverse of K) '''
+    P1, P2 = get_projection_matrices_from_essential(E, points_image_1, points_image_2)
+    hom_points_3d = np.array([compute_3d_point(P1, P2, point1, point2) for point1, point2 in zip(points_image_1, points_image_2)])
+    points_3d = hom_points_3d / hom_points_3d[:,-1,np.newaxis]
+
+    points = np.array(list(zip(points_3d[:,0].ravel(), points_3d[:,1].ravel(), points_3d[:,2].ravel())),dtype=[('x','f4'), ('y','f4'),('z', 'f4')])
+    el = PlyElement.describe(points, 'vertex')
+    PlyData([el]).write(filename)
+    
+#task 1
+
+F = fundamental_matrix(points_image_1, points_image_2)
+
+#task 2
+
+im = Image.open('data/kronan1.JPG')
+draw = ImageDraw.Draw(im)
+
+
+for i in range(20):
+    draw = draw_epipolar_line(points_image_2[i,:], F, draw, im.size[0])
+width = 3
+for point in points_image_1[:20,:]:
+    x = point[0]
+    y = point[1]
+    draw.ellipse((x - width, y - width, x + width, y + width), fill = 1000)
+    draw.point(point[:-1], fill = 1000)
+im.save('data/kronan1_epipoles.JPG', 'JPEG')
+
+im = Image.open('data/kronan2.JPG')
+draw = ImageDraw.Draw(im)
+
+for i in range(20):
+    draw = draw_epipolar_line(points_image_2[i,:], F.T, draw, im.size[0])
+im.save('data/kronan2_epipoles.JPG', 'JPEG')
+
+#task 3
+
+T1 = get_normalization_matrix(points_image_1)
+T2 = get_normalization_matrix(points_image_2)
+
+norm_points1 = T1.dot(points_image_1.T).T
+norm_points2 = T2.dot(points_image_2.T).T
+
+F = fundamental_matrix(norm_points1, norm_points2)
+
+F = T1.T.dot(F.dot(T2))
+
+im = Image.open('data/kronan1.JPG')
+draw = ImageDraw.Draw(im)
+
+
+for i in range(20):
+    draw = draw_epipolar_line(points_image_2[i,:], F, draw, im.size[0])
+for point in points_image_1[:20,:]:
+    x = point[0]
+    y = point[1]
+    draw.ellipse((x - width, y - width, x + width, y + width), fill = 1000)
+    draw.point(point[:-1], fill = 1000)
+im.save('data/kronan1_norm_epipoles.JPG', 'JPEG')
+
+im = Image.open('data/kronan2.JPG')
+draw = ImageDraw.Draw(im)
+
+
+for i in range(20):
+    draw = draw_epipolar_line(points_image_1[i,:], F.T, draw, im.size[0])
+im.save('data/kronan2_norm_epipoles.JPG', 'JPEG')
+
+# task 4
+
+K = io.loadmat('data/compEx3data.mat')['K']
+E = essential_matrix(points_image_1, points_image_2, K)
+new_F = np.linalg.inv(K.T).dot(E.dot(np.linalg.inv(K)))
+
+im = Image.open('data/kronan1.JPG')
+draw = ImageDraw.Draw(im)
+
+for i in range(20):
+    draw = draw_epipolar_line(points_image_2[i,:], new_F, draw, im.size[0])
+for point in points_image_1[:20,:]:
+    x = point[0]
+    y = point[1]
+    draw.ellipse((x - width, y - width, x + width, y + width), fill = 1000)
+    draw.point(point[:-1], fill = 1000)
+im.save('data/kronan1_norm_ess_epipoles.JPG', 'JPEG')
+
+im = Image.open('data/kronan2.JPG')
+draw = ImageDraw.Draw(im)
+
+for i in range(20):
+    draw = draw_epipolar_line(points_image_1[i,:], new_F.T, draw, im.size[0])
+im.save('data/kronan2_norm_ess_epipoles.JPG', 'JPEG')
+
+
+#task 5
+
+norm_points1 = np.linalg.inv(K).dot(points_image_1.T).T
+norm_points2 = np.linalg.inv(K).dot(points_image_2.T).T
+triangulate_points(E, norm_points2, norm_points1, 'pc.ply')
