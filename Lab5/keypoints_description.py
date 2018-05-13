e6edfbbcca1928595e572e2f39f2519a66014e0e
samor <dingurth.d@gmail.com>
2018-05-13 18:43:09 +0200

    Lab5 task 1 done

---
+++ Lab5/keypoints_description.py
@@ -0,0 +1,63 @@
+import numpy as np
+from PIL import Image, ImageDraw
+from scipy import ndimage as ndim
+import imageio
+import matplotlib.pyplot as plt
+import time
+import skimage.feature
+from scipy import io
+
+data = io.loadmat('data/Episcopal_Gaudi/f_o.mat')
+points1 = np.concatenate([data['y1'], data['x1']], axis=1)
+points2 = np.concatenate([data['y2'], data['x2']], axis=1)
+
+image = np.asarray(Image.open('data/Episcopal_Gaudi/1_o.jpg', 'r').convert('L')).astype(np.float)
+
+def compute_orientations_histogram(image, point, take_scale=False, sigma=None):
+    coeff = 1.
+    if take_scale:
+        assert sigma is not None, 'For taking scale into account the sigma parameter is needed!'
+        coeff = sigma * 1.5
+
+    point = point.astype(np.int)
+    top_pad = 0
+    bottom_pad = 0
+    left_pad = 0
+    right_pad = 0
+    if point[0] < 20:
+        point[0] += 20
+        top_pad = 20
+    top = point[0] - 20
+    bottom = point[0] + 21
+
+    if point[1] < 20:
+        point[1] += 20
+        left_pad = 20
+    left = point[1] - 20
+    right = point[1] + 21
+
+    if bottom >= image.shape[0]:
+        bottom_pad = 20
+    if right >= image.shape[1]:
+        right_pad = 20
+
+    grad = np.pad(np.stack(np.gradient(image), axis=-1), ((top_pad, bottom_pad), (left_pad, right_pad), (0,0)), 'reflect')[top:bottom, left:right]
+
+    ax = np.arange(-41 // 2 + 1., 41 // 2 + 1.)
+    xx, yy = np.meshgrid(ax, ax)
+
+    kernel = np.exp(-(xx**2 + yy**2) / (2. * coeff**2))
+    kernel /= np.sum(kernel)
+
+    magnitude = np.sqrt(np.sum(grad ** 2, axis=-1)) * kernel
+    orientation = (36. * (np.arctan2(grad[:,:,1], grad[:,:,0]) + np.pi) / (2. * np.pi)).astype(np.int)
+    orientation[orientation == 36.] = 0.
+    histogram = np.array([np.sum(magnitude[orientation == i]) for i in range(36)])
+
+    return histogram
+
+t = time.time()
+for point in points1:
+    compute_orientations_histogram(image,point)
+
+print('Time for {} points:'.format(points1.shape[0]), time.time() - t)
